0757e8330b2bae07b72a3a5eceaab00fc25ff38b
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-11 16:48:43 -0500

    agregando debugging de los estados de la aplicacion y agregando metodo para el guardado del estado

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,10 +1,12 @@
 ***REMOVED***
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +15,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -48,7 +53,12 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        Log.d(TAG,"onCreate(Bundle) called");
+        setContentView(R.layout.content_quiz);
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    ***REMOVED***
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -77,18 +87,13 @@ public class QuizActivity extends AppCompatActivity {
     ***REMOVED***);
 
         updateQuestion();
+***REMOVED***
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-        ***REMOVED***
-    ***REMOVED***);
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
 ***REMOVED***
 
     @Override
@@ -111,5 +116,29 @@ public class QuizActivity extends AppCompatActivity {
     ***REMOVED***
 
         return super.onOptionsItemSelected(item);
+***REMOVED***
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+***REMOVED***
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+***REMOVED***@Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+***REMOVED***@Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+***REMOVED***@Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
 ***REMOVED***
 ***REMOVED***
